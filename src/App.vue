2cf55487b49bc7c2a2dfde1d97ab8201ceb41f04
vladik-png <vladsemotuk42@gmail.com>
2025-01-30 18:34:39 +0200

    create footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
     <MainMenu />
     <router-view></router-view>
+    <FooterDown />
   </div>
 </template>
 
 <script>
 import MainMenu from './components/MainMenu.vue';
+import FooterDown from './components/FooterDown.vue';
 
 export default {
   name: 'App',
   components: {
     MainMenu,
-  },
+    FooterDown
+  }
 };
+
 </script>
 
 <style>
